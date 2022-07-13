1a62ea10261d08444160249dd80851ea73b23493
Max Spahn <m.spahn@tudelft.nl>
2022-07-13 09:43:31 +0200

    Adds lfs fetch and checkout.

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -6,4 +6,6 @@ sudo rosdep init || echo "rosdep was initialized already, continue regardlessly"
 rosdep update && rosdep install --from-paths src --ignore-src -y
 catkin build && source devel/setup.bash
 roscd fabrics_bridge/test
+git lfs fetch
+git lfs checkout
 chmod +x test_fabrics_node && ./test_fabrics_node
