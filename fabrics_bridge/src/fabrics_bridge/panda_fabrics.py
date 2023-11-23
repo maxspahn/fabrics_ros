79cbc6a5cc2adc52650a5c9c2918fc5088f193d1
Max Spahn <m.spahn@tudelft.nl>
2023-11-23 14:49:22 +0100

    ft[structure]: Moves generic fabrics node to src so it can be derived.

---
+++ fabrics_bridge/src/fabrics_bridge/panda_fabrics.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import numpy as np
+
+import rospy
+import rospkg
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+
+from urdf_parser_py.urdf import URDF
+
+from fabrics_bridge.generic_fabrics_node import GenericFabricsNode
+from forwardkinematics.urdfFks.generic_urdf_fk import GenericURDFFk
+
+class PandaFabricsNode(GenericFabricsNode):
+    def __init__(self):
+        super().__init__('panda_fabrics_node')
+
+    def init_robot_specifics(self):
+        self.joint_names = [f'panda_joint{i+1}' for i in range(7)]
+        self._action = np.zeros(7)
+        rospack = rospkg.RosPack()
+        self._planner_folder = rospack.get_path("fabrics_bridge") + "/planner/panda/"
+        self._forward_kinematics = GenericURDFFk(
+            self.urdf,
+            rootLink=rospy.get_param("/root_link"),
+            end_link=rospy.get_param("/end_links"),
+        )
+        self._planner_type = 'holonomic'
+        # get the joint limits for each joint
+        robot = URDF.from_parameter_server(rospy.get_param("/urdf_source"))
+        self.joint_limits = []
+        for i in self.joint_names:
+            self.joint_limits.append(
+                [
+                    robot.joint_map[i].limit.lower,
+                    robot.joint_map[i].limit.upper,
+                ]
+            )
+
+    def init_publishers(self):
+        self._panda_command_publisher = rospy.Publisher(
+            '/panda_joint_velocity_controller/command',
+            Float64MultiArray,
+            queue_size=10
+        )
+
+    def init_joint_states_subscriber(self):
+        self._q = np.zeros(7)
+        self._qdot = np.zeros(7)
+        self.joint_state_subscriber = rospy.Subscriber(
+            "/joint_states_filtered",
+            JointState,
+            self.joint_states_callback,
+            tcp_nodelay=True,
+        )
+
+    def set_joint_states_values(self):
+        self._runtime_arguments['q'] = self._q
+        self._runtime_arguments['qdot'] = self._qdot
+
+    def joint_states_callback(self, msg: JointState):
+        self._q = np.array(msg.position[5:12])
+        self._qdot = np.array(msg.velocity[5:12])
+
+    def publish_action(self):
+        action_msg = Float64MultiArray(data=self._action)
+        self._panda_command_publisher.publish(action_msg)
+
+if __name__ == "__main__":
+    node = PandaFabricsNode()
+    try:
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
+
---
+++ fabrics_bridge/src/fabrics_bridge/utils.py
@@ -0,0 +1,205 @@
+import math
+import numpy as np
+import rospy
+
+from fabrics_msgs.msg import FabricsObstacle
+from geometry_msgs.msg import Point, Quaternion, Vector3
+
+from fabrics.planner.parameterized_planner import ParameterizedFabricPlanner
+from fabrics.planner.non_holonomic_parameterized_planner import NonHolonomicParameterizedFabricPlanner
+
+def create_planner(planner_type: str, forward_kinematics) -> ParameterizedFabricPlanner:
+    base_energy: str = (
+        "0.5 * 0.2 * ca.dot(xdot, xdot)"
+    )
+    collision_geometry: str = (
+        "-0.5 / (x ** 2) * xdot ** 2"
+    )
+    collision_finsler: str = (
+        "0.1/(x**2) * (-0.5 * (ca.sign(xdot) - 1)) * xdot**2"
+    )
+    limit_geometry: str = (
+        "-0.50 / (x ** 2) * xdot ** 2"
+    )
+    limit_finsler: str = (
+        "0.4/(x**1) * (-0.5 * (ca.sign(xdot) - 1)) * xdot**2"
+    )
+    self_collision_geometry: str = (
+        "-0.5 / (x ** 1) * (-0.5 * (ca.sign(xdot) - 1)) * xdot ** 2"
+    )
+    self_collision_finsler: str = (
+        "0.1/(x**2) * xdot**2"
+    )
+    attractor_potential: str = (
+        "5.0 * (ca.norm_2(x) + 1 / 10 * ca.log(1 + ca.exp(-2 * 10 * ca.norm_2(x))))"
+    )
+    attractor_metric: str = (
+        "((2.0 - 0.3) * ca.exp(-1 * (0.75 * ca.norm_2(x))**2) + 0.3) * ca.SX(np.identity(x.size()[0]))"
+    )
+    degrees_of_freedom = rospy.get_param("/degrees_of_freedom")
+    if planner_type == "holonomic":
+        return ParameterizedFabricPlanner(
+            degrees_of_freedom,
+            forward_kinematics,
+            collision_geometry=collision_geometry,
+            collision_finsler=collision_finsler,
+            self_collision_finsler=self_collision_finsler,
+            limit_geometry=limit_geometry,
+            limit_finsler=limit_finsler,
+        )
+    if planner_type == "nonholonomic":
+        collision_geometry: str = (
+            "-sym('k_geo_col') / (x ** 1) * xdot ** 2"
+        )
+        collision_finsler: str = (
+            "sym('k_fin_col')/(x**1) * (-0.5 * (ca.sign(xdot) - 1)) * xdot**2"
+        )
+        return NonHolonomicParameterizedFabricPlanner(
+            degrees_of_freedom,
+            forward_kinematics,
+            collision_geometry=collision_geometry,
+            collision_finsler=collision_finsler,
+            self_collision_finsler=self_collision_finsler,
+            limit_geometry=limit_geometry,
+            limit_finsler=limit_finsler,
+            l_offset=rospy.get_param("/l_offset"),
+        )
+
+
+# Helpers
+def _it(self):
+    yield self.x
+    yield self.y
+    yield self.z
+
+
+Point.__iter__ = _it
+Vector3.__iter__ = _it
+
+
+def _it(self):
+    yield self.x
+    yield self.y
+    yield self.z
+    yield self.w
+
+
+Quaternion.__iter__ = _it
+
+
+def sphere_obs(
+    x: float, y: float, z: float, r: float = 0.05
+) -> FabricsObstacle:
+    o = FabricsObstacle()
+    o.header.frame_id = "panda_link0"
+    o.radius = r
+    o.position = Point(x, y, z)
+    o.obstacle_type = "sphere"
+    return o
+
+def quaternion_to_rotation_matrix(
+    quaternion: np.ndarray, ordering: str = "wxyz"
+) -> np.ndarray:
+    # Normalize the quaternion if needed
+    quaternion /= np.linalg.norm(quaternion)
+
+    if ordering == "wxyz":
+        w, x, y, z = quaternion
+    elif ordering == "xyzw":
+        x, y, z, w = quaternion
+    else:
+        raise InvalidQuaternionOrderError(
+            f"Order {ordering} is not permitted, options are 'xyzw', and 'wxyz'"
+        )
+    rotation_matrix = np.array(
+        [
+            [
+                1 - 2 * y**2 - 2 * z**2,
+                2 * x * y - 2 * w * z,
+                2 * x * z + 2 * w * y,
+            ],
+            [
+                2 * x * y + 2 * w * z,
+                1 - 2 * x**2 - 2 * z**2,
+                2 * y * z - 2 * w * x,
+            ],
+            [
+                2 * x * z - 2 * w * y,
+                2 * y * z + 2 * w * x,
+                1 - 2 * x**2 - 2 * y**2,
+            ],
+        ]
+    )
+
+    return rotation_matrix
+
+def Rx(theta):
+  return np.matrix([[ 1, 0           , 0           ],
+                   [ 0, math.cos(theta),-math.sin(theta)],
+                   [ 0, math.sin(theta), math.cos(theta)]])
+
+def Ry(theta):
+  return np.matrix([[ math.cos(theta), 0, math.sin(theta)],
+                   [ 0           , 1, 0           ],
+                   [-math.sin(theta), 0, math.cos(theta)]])
+
+def Rz(theta):
+  return np.matrix([[ math.cos(theta), -math.sin(theta), 0 ],
+                   [ math.sin(theta), math.cos(theta) , 0 ],
+                   [ 0           , 0            , 1 ]])
+
+def rectangle_spheres(origin, w, h, r, extra_s = 0):
+    # Method to determine a rectangle of sphere with a certain radius qaually distributes on the perimeter
+    perimeter = 2*h+2*w
+    # n_spheres = int((perimeter + 2*r)/(2*r) - 1 + extra_s)
+    n_spheres = 10
+    rot_matrix = Rz(math.pi/2)
+
+    # Initialize matrix containing sphere points
+    sphere_loc = np.zeros((3, n_spheres))
+    dist = perimeter/float(n_spheres)
+    for i in range(n_spheres):
+        # x location of the sphere, unwrapped rectangle into a line
+        sphere_loc[0,i] = dist*i
+
+    for i in range(n_spheres):
+        if sphere_loc[0,i] > w:
+            # Translate to origin
+            sphere_loc[0,i] -= w
+            # Rotate
+            sphere_loc[:,i] =  np.matmul(rot_matrix, sphere_loc[:,i])
+            # Translate back
+            sphere_loc[0,i] += w
+
+    for i in range(n_spheres):
+        if sphere_loc[1,i] > h:
+            sphere_loc[0,i] -= w
+            sphere_loc[1,i] -= h
+            sphere_loc[:,i] =  np.matmul(rot_matrix, sphere_loc[:,i])
+            sphere_loc[0,i] += w
+            sphere_loc[1,i] += h
+
+    for i in range(n_spheres):
+        if sphere_loc[0,i] < 0:
+            sphere_loc[1,i] -= h
+            # Rotate
+            sphere_loc[:,i] =  np.matmul(rot_matrix, sphere_loc[:,i])
+            # Translate back
+            sphere_loc[1,i] += h
+
+    # Translate origin in the center of rectangle
+    sphere_loc[0,:] = sphere_loc[0,:] - w/2 + origin[0]
+    sphere_loc[1,:] = sphere_loc[1,:] - h/2 + origin[1]
+    sphere_loc[2,:] = sphere_loc[2,:] + origin[2]
+
+    return sphere_loc.T
+
+def pose_to_transformation_matrix(
+    position: np.ndarray,
+    quaternion: np.ndarray,
+    ordering: str = "wxyz"
+) -> np.ndarray:
+    transform_matrix = np.identity(4)
+    transform_matrix[0:3,3] = position
+    transform_matrix[0:3,0:3] = quaternion_to_rotation_matrix(quaternion, ordering=ordering)
+    return transform_matrix
